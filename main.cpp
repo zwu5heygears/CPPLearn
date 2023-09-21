cb2e3e4f3ccb10831d9cae1ce25a0def0a4f9cc3
zwu5 <zwu5@heygears.com>
2023-09-21 18:00:16 +0800

    test

--- main.cpp
+++ main.cpp
@@ -3,20 +3,12 @@
 #include <stdint.h>
 #include <vector>
 #include <iostream>
+// hello
+/* hello */
+
+using namespace std;
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
-std::vector<char> m1;
-m1.push_back(0x00);
-m1.push_back(0x00);
-m1.push_back(0x80);
-m1.push_back(0x3f);
-    char tmp[4] = { 0 };
-tmp[0] = m1[0] & 0xFF;
-tmp[1] = m1[1] & 0xFF;
-tmp[2] = m1[2] & 0xFF;
-tmp[3] = m1[3] & 0xFF;
-float value = 0;
-printf("%f", value);
+    cout << "Hello heygears" << endl;
     return 0;
 }
