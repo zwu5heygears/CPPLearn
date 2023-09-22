8b1cf9e472457f505ae304b392094dc287851c50
zwu5 <zwu5@heygears.com>
2023-09-22 09:34:00 +0800

    01-10

--- main.cpp
+++ main.cpp
@@ -5,10 +5,33 @@
 #include <iostream>
 // hello
 /* hello */
-
-using namespace std;
+#define MAX 200
+using namespace std;  // const
+const int MAX2 = 300;
 
 int main() {
+    int v2_; // letter number _ (case)
+    int a = 0;  // variable use for manage memory
+    cout << a << endl;
+    a = 1;
+    cout << a << endl;
+    double b = static_cast<double>(a);
+
+    // 数据类型
+    cout << sizeof(a) << endl;
+    cout << sizeof(short) << sizeof(int) << sizeof(long) <<sizeof(long long) << endl;
+    cout << sizeof(ushort) << sizeof(uint) << sizeof(ulong) << sizeof(unsigned long long) << endl;
+    cout << sizeof(wchar_t) << sizeof(char) << sizeof(char16_t) << sizeof(char32_t) << endl;
+
+    cout << sizeof(float) << sizeof(double) << endl; // 7位有效数字  15-16位有效数字
+    float f1 = 3.141592657f;
+    double d1 = 3.141592657;
+    cout << d1 << endl;  // 默认输出5位小数
+    cout << f1 << endl;
+
+    float f2 = 3e-3;
+    cout <<f2 <<endl;
     cout << "Hello heygears" << endl;
+
     return 0;
 }
